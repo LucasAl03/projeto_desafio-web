a70251151e369133d021b5b013995d727d3f21fc
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-09 21:47:05 -0300

    Adicionado o código no arquivo script.js para o formulário

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-//Pegando elementos do 
-const formPessoa - document.querySelector ("#formulario-pessoa")
+//Pegando elementos do dom
+const formPessoa = document.querySelector ("#formulario-pessoa")
 const divDados = document.querySelector ("#div-dados")
+
+formPessoa.addEventListener('submit', (evt) =>{
+    evt.preventDefault()
+
+    const dadosForm = new FormData(formPessoa)
+
+    const nome = dadosForm.get('nome')
+    const idade = dadosForm.get('idade')
+
+    let situacaoIdade = idade >= 18 ? `${nome}, é Maior de Idade`: `${nome}, é Menor de Idade`
+
+    divDados.innerHTML = situacaoIdade
+
+})
