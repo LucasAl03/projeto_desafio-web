beae9d37acc73e56e78fd776f104c5b866cf919a
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 20:04:56 -0300

    Adicionado codigo ao arquivo script_fundamentos.js

---
+++ js/script_fundamentos.js
@@ -0,0 +1,42 @@
+//Comentário de uma linha
+
+/*
+    Comentário de múltiplas linhas
+    É possível ter várias
+    linhas
+*/
+
+console.log("Olá Mundo")
+
+//Declaração de Variável
+let num = 10
+var num2 = 40
+const num3 = 300
+const num4 = 100
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+console.log(num4 + num)
+console.log(num4 * num)
+console.log(num2 + num * num2)
+
+if (true){
+    let num5 = 250
+    console.log(num5)
+
+    var num6 = 13
+    
+}
+
+console.log(num6)
+
+num = 1802
+console.log(num)
+
+num2 = 1700
+console.log(num2)
+
+//não e possivel alterar uma const
+num3 = 900
+console.log(num3)
