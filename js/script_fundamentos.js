35d7e7d0761ac011fb4472d017c9620ef1fd5e55
Lucas ALberto <lucas.a.santos126@aluno.senai.br>
2026-06-10 21:07:45 -0300

    Adicionado declaração de variáveis concatenação operadores matemáticos e testes lógicos

--- js/script_fundamentos.js
+++ js/script_fundamentos.js
@@ -38,5 +38,29 @@ num2 = 1700
 console.log(num2)
 
 //não e possivel alterar uma const
-num3 = 900
+//num3 = 900
 console.log(num3)
+
+//Concatenação
+console.log('Valor da variável num3 é ' + num3)
+console.log("Valor da variável num3 é " + num3)
+console.log('Valor da variável num3 é ' , num3)
+console.log(`Valor da variável num3 é ${num3}`)
+
+//Operadores matemáticos
+
+/*
+    + soma
+    - subtração
+    * multiplicação
+    / divisão
+    % módulo - Resto da dicisão
+*/
+
+console.log('A soma dos números é ', 12 + 8)
+console.log('A subtração entre variáveis ', num2 - num3)
+console.log('A multiplicação dos números ', num2 * 5)
+console.log('A divisão entre os valores ', num3 / num2)
+console.log(`O resto da divisão entre 10 e 2 é: ${10 % 2}`)
+
+//Teste Lógico
